3d795525bdf3bba5c64600cec6418e2d2313926f
francovanney <franco.vanney@incluit.com>
2023-02-15 20:01:50 -0300

    Slider Skills Logos

--- src/components/Body/Skills/index.jsx
+++ src/components/Body/Skills/index.jsx
@@ -1,16 +1,169 @@
 import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import ReactLogo from "../../../assets/Logos/React-icon.svg";
+import HtmlLogo from "../../../assets/Logos/html-1.svg";
+import CssLogo from "../../../assets/Logos/css-3.svg";
+import JsLogo from "../../../assets/Logos/logo-javascript.svg";
+import FigmaLogo from "../../../assets/Logos/figma-5.svg";
+import BootstrapLogo from "../../../assets/Logos/bootstrap-5-1.svg";
+import NpmLogo from "../../../assets/Logos/npm-square-red-1.svg";
+import AdobeXD from "../../../assets/Logos/adobe-xd-1.svg";
+import AdobePS from "../../../assets/Logos/adobe-photoshop-2.svg";
+import Jira from "../../../assets/Logos/jira-1.svg";
+import Postman from "../../../assets/Logos/postman.svg";
+import VsCode from "../../../assets/Logos/visual-studio-code-1.svg";
+//import NpmLogo from "../../../assets/Logos/npm-square-red-1.svg";
 
 const Skills = () => {
+	const responsive = {
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 5,
+			slidesToSlide: 1 // optional, default to 1.
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 4,
+			slidesToSlide: 1 // optional, default to 1.
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 2,
+			slidesToSlide: 1 // optional, default to 1.
+		}
+	};
 	return (
-		<Container>
-			<Row>
-				<Col>
-					<h2>Skills</h2>
-				</Col>
-			</Row>
-		</Container>
+		<section className='page-section'>
+			<Container>
+				<Row>
+					<Col className='mt-4 mb-4'>
+						<h2>Skills</h2>
+					</Col>
+					<Col>
+						<Carousel
+							//className="justify-content-center"
+							swipeable={false}
+							draggable={true}
+							showDots={false}
+							responsive={responsive}
+							autoPlay={true}
+							ssr={true} // means to render carousel on server-side.
+							infinite={true}
+							autoPlaySpeed={900}
+							transitionDuration={900}
+							arrows={false}>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										HtmlLogo
+									}
+									width='80'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										CssLogo
+									}
+									width='80'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										JsLogo
+									}
+									width='80'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										ReactLogo
+									}
+									width='80'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										NpmLogo
+									}
+									width='80'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										FigmaLogo
+									}
+									width='150'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										BootstrapLogo
+									}
+									width='100'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										AdobeXD
+									}
+									width='100'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										AdobePS
+									}
+									width='100'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										BootstrapLogo
+									}
+									width='100'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										Jira
+									}
+									width='100'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										VsCode
+									}
+									width='100'></img>
+							</div>
+							<div>
+								<img
+									className='img-fluid'
+									src={
+										Postman
+									}
+									width='100'></img>
+							</div>
+						</Carousel>
+					</Col>
+				</Row>
+			</Container>
+		</section>
 	);
 };
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,38 +1,44 @@
 import { Nav } from "react-bootstrap";
 import logo from "../../assets/logo_fv-dark.png";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
-	const nav = document.querySelector("#nav");
+	const [scrolled, setScrolled] = useState(false);
 
-	const onScroll = (event) => {
-		const scrollPosition = event.target.scrollingElement.scrollTop;
+	const onScroll = () => {
+		const scrollPosition = window.pageYOffset;
 		if (scrollPosition > 10) {
-			if (!nav.classList.contains("scrolled-down")) {
-				nav.classList.add("scrolled-down");
+			if (!scrolled) {
+				setScrolled(true);
 			}
 		} else {
-			if (nav.classList.contains("scrolled-down")) {
-				nav.classList.remove("scrolled-down");
+			if (scrolled) {
+				setScrolled(false);
 			}
 		}
 	};
 
-	document.addEventListener("scroll", onScroll);
-
-	
+	useEffect(() => {
+		window.addEventListener("scroll", onScroll);
+		return () => {
+			window.removeEventListener("scroll", onScroll);
+		};
+	}, [scrolled]);
 	return (
 		<body>
-			<Nav id='nav'>
-				<div class='container flex'>
+			<nav
+				className={scrolled ? "scrolled-down" : ""}
+				id='nav'>
+				<div className='container flex'>
 					<img src={logo} />
-					<div class='links'>
+					<div className='links'>
 						<Nav.Link>Lorem1</Nav.Link>
 						<Nav.Link>Lorem2</Nav.Link>
 						<Nav.Link>Lorem3</Nav.Link>
 						<Nav.Link>Lorem4</Nav.Link>
 					</div>
 				</div>
-			</Nav>
+			</nav>
 		</body>
 	);
 };
